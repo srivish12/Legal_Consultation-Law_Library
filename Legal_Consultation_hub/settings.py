9597ca7e89d19b6b372ee550ab8a31f72c6691ea
srivish12 <nalincha30@gmail.com>
2026-01-31 02:30:12 +0000

    region error corrected

--- Legal_Consultation_hub/settings.py
+++ Legal_Consultation_hub/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #DEBUG = True
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost',
+ALLOWED_HOSTS = ['localhost',
                   '127.0.0.1',
                   'legal-consultation-hub-6742f22a8314.herokuapp.com',
                  ]
@@ -228,7 +228,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'legal-consultation-hub' #  AWS bucket name
-    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
