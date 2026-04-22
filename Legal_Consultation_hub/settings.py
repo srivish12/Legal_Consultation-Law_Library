2f46e7c9d5df96ad990f0d73e88bb5d066a68054
srivish12 <nalincha30@gmail.com>
2026-04-22 11:21:50 +0100

    server error500 test 4

--- Legal_Consultation_hub/settings.py
+++ Legal_Consultation_hub/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+
 
 ALLOWED_HOSTS = [
     'localhost',
